b65b96e04dc38490da24971ac28c895d53ec0cc6
shreecheryl <shreecheryl@att.net>
2023-04-06 15:47:05 -0700

    practice thru step 7 complete

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,18 +1,84 @@
 //STEP 1
+// let movies = ["Lars and the Real Girl", "Rocky Horror Picture Show", "Out of Africa", "Terms of Endearment", "The Woman King"];
+// console.log(movies[1]);
 
 //STEP 2
+// let movies2 = new Array(5);
+// movies2[0] = "Lars and the Real Girl";
+// movies2[1] = "Rocky Horror Picture Show";
+// movies2[2] = "Out of Africa";
+// movies2[3] = "Terms of Endearment";
+// movies2[4] = "The Woman King";
+// console.log(movies2[0]);
 
 //STEP 3
+// let movies2 = new Array(5);
+// movies2[0] = "Lars and the Real Girl";
+// movies2[1] = "Rocky Horror Picture Show";
+// movies2[2] = "Out of Africa";
+// movies2[3] = "Terms of Endearment";
+// movies2[4] = "The Woman King";
+// movies2.splice(3, 0, "Avatar")
+// console.log(movies2.length)
 
 //STEP 4
+// let movies3 = [];
+// movies3[0] = "Lars and the Real Girl";
+// movies3[1] = "Rocky Horror Picture Show";
+// movies3[2] = "Out of Africa";
+// movies3[3] = "Terms of Endearment";
+// movies3[4] = "The Woman King";
+// delete movies3[0];
+// console.log(movies3);
 
 //STEP 5
+// let movies4 = [];
+// movies4[0] = "Lars and the Real Girl";
+// movies4[1] = "Rocky Horror Picture Show";
+// movies4[2] = "Out of Africa";
+// movies4[3] = "Terms of Endearment";
+// movies4[4] = "The Woman King";
+// movies4[5] = "Avatar"
+// movies4[6] = "Paris, Texas"
+// for (movie in movies4) {
+//     console.log(movies4[movie]);
+// }
 
 //STEP 6
+// let movies4 = [];
+// movies4[0] = "Lars and the Real Girl";
+// movies4[1] = "Rocky Horror Picture Show";
+// movies4[2] = "Out of Africa";
+// movies4[3] = "Terms of Endearment";
+// movies4[4] = "The Woman King";
+// movies4[5] = "Avatar"
+// movies4[6] = "Paris, Texas"
+// for (movie of movies4) {
+//     console.log(movie);
+// }
 
 //STEP 7
+// let movies4 = [];
+// movies4[0] = "Lars and the Real Girl";
+// movies4[1] = "Rocky Horror Picture Show";
+// movies4[2] = "Out of Africa";
+// movies4[3] = "Terms of Endearment";
+// movies4[4] = "The Woman King";
+// movies4[5] = "Avatar"
+// movies4[6] = "Paris, Texas"
+// for (movie of movies4.sort()) {
+//     console.log(movie);
+// }
 
 //STEP 8
+let movies4 = [];
+movies4[0] = "Lars and the Real Girl";
+movies4[1] = "Rocky Horror Picture Show";
+movies4[2] = "Out of Africa";
+movies4[3] = "Terms of Endearment";
+movies4[4] = "The Woman King";
+movies4[5] = "Avatar"
+movies4[6] = "Paris, Texas"
 
 //STEP 9
 
